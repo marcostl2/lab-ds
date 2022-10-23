1ca0c7830e3f734ef70c06feb004a5f0aa665812
marcostl2 <marcossilvatl2@gmail.com>
2022-10-23 15:36:20 -0400

    refactor: removing storybook folder

--- storybook-static/assets/preview.6e5c655e.js
+++
@@ -1,2 +0,0 @@
-import{t as o}from"./index.42592ccd.js";import"./iframe.e63ee495.js";import"./es.map.constructor.f22c34b7.js";import"./es.number.to-fixed.7a8fa499.js";import"./index.4db96a8d.js";const s={actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}},docs:{theme:o.dark}};export{s as parameters};
-//# sourceMappingURL=preview.6e5c655e.js.map
